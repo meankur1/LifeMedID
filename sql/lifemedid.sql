10d5f811d675e61b791f157e29cfe6b48349e26b
arkoghosh11 <Bloom1289>
2016-06-16 15:07:03 -0500

    Updated with new columns and dummy data

--- sql/lifemedid.sql
+++ sql/lifemedid.sql
@@ -23,20 +23,23 @@ DROP TABLE IF EXISTS `applicants`;
 CREATE TABLE `applicants` (
   `applicant_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `id` varchar(100) NOT NULL,
+  `suffix` varchar(5) DEFAULT NULL,
   `first_name` varchar(100) NOT NULL,
+  `middle_name` varchar(100) DEFAULT NULL,
   `last_name` varchar(100) NOT NULL,
+  `level_of_assurance` varchar(20) DEFAULT NULL,
   `date_of_birth` varchar(10) NOT NULL,
   `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `lifemed_id` bigint(20) NOT NULL,
   PRIMARY KEY (`applicant_id`),
   UNIQUE KEY `FK_lifemed_id` (`lifemed_id`),
-  CONSTRAINT `FK_lifemed_id` FOREIGN KEY (`lifemed_id`) REFERENCES `lifemedids` (`lifemed_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `FK_lifemed_id` FOREIGN KEY (`lifemed_id`) REFERENCES `lifemedid_transactions` (`lifemed_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=65537 DEFAULT CHARSET=latin1;
 
 /*Data for the table `applicants` */
 
-insert  into `applicants`(`applicant_id`,`id`,`first_name`,`last_name`,`date_of_birth`,`created_date`,`updated_date`,`lifemed_id`) values (1,'id1','first_name1','last_name1','date_of_bi','2016-06-09 18:08:32','2016-06-09 18:08:47',1),(2,'id2','first_name2','last_name2','date_of_bi','2016-06-09 18:08:32','2016-06-09 18:08:48',2),(3,'id3','first_name3','last_name3','date_of_bi','2016-06-09 18:08:32','2016-06-09 18:08:49',3),(4,'id4','first_name4','last_name4','date_of_bi','2016-06-09 18:08:32','2016-06-09 18:08:51',4),(65536,'12345','Freddy','Flintstone','1978-11-22','2016-06-10 15:11:50','2016-06-10 15:11:50',65536);
+insert  into `applicants`(`applicant_id`,`id`,`suffix`,`first_name`,`middle_name`,`last_name`,`level_of_assurance`,`date_of_birth`,`created_date`,`updated_date`,`lifemed_id`) values (1,'id1',NULL,'first_name1',NULL,'last_name1','level1','date_of_bi','2016-06-09 18:08:32','2016-06-16 12:12:23',1),(2,'id2',NULL,'first_name2',NULL,'last_name2','level1','date_of_bi','2016-06-09 18:08:32','2016-06-16 12:12:27',2),(3,'id3',NULL,'first_name3',NULL,'last_name3','level1','date_of_bi','2016-06-09 18:08:32','2016-06-16 12:12:28',3),(4,'id4',NULL,'first_name4',NULL,'last_name4','level1','date_of_bi','2016-06-09 18:08:32','2016-06-16 12:12:30',4),(65536,'12345',NULL,'Freddy',NULL,'Flintstone','level1','1978-11-22','2016-06-10 15:11:50','2016-06-16 12:12:36',65536);
 
 /*Table structure for table `hibernate_sequences` */
 
@@ -57,6 +60,7 @@ DROP TABLE IF EXISTS `idpdocuments`;
 
 CREATE TABLE `idpdocuments` (
   `idp_document_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `suffix` varchar(5) DEFAULT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `middle_name` varchar(100) DEFAULT NULL,
@@ -77,13 +81,13 @@ CREATE TABLE `idpdocuments` (
 
 /*Data for the table `idpdocuments` */
 
-insert  into `idpdocuments`(`idp_document_id`,`first_name`,`last_name`,`middle_name`,`date_of_birth`,`issuing_authority`,`document_issuer`,`document_expiration_date`,`document_type`,`document_title`,`document_number`,`created_date`,`updated_date`,`applicant_id`) values (1,'first_name1','last_name1',NULL,'1988-11-26','NY','NY','2018-12-25','A','Drivers License','FF-234-123-1231','2016-06-09 18:07:30','2016-06-10 02:53:06',1),(2,'first_name2','last_name2',NULL,'1988-11-26','NY','NY','2018-12-25','A','Drivers License','FF-234-123-1231','2016-06-09 18:07:30','2016-06-10 02:53:08',2),(3,'first_name3','last_name3','middle3','1988-11-26','NY','NY','2018-12-25','A','Drivers License','FF-234-123-1231','2016-06-09 18:07:30','2016-06-10 02:53:10',3),(4,'first_name4','last_name4',NULL,'1988-11-26','NY','NY','2018-12-25','B','SSN Card','FF-234-123-1231','2016-06-09 18:07:30','2016-06-10 02:53:15',4),(5,'first_name5','last_name5',NULL,'1988-04-21','TX','TX','2018-06-06','B2','IDCard','FF-234-123-1232','2016-06-10 14:38:32','2016-06-10 14:38:41',4),(65536,'Frederick','Flintstone','J','1978-11-22','IL','IL','2018-04-13','A1','Passport','PP-465-67-6546','2016-06-10 15:11:50','2016-06-10 15:11:50',65536);
+insert  into `idpdocuments`(`idp_document_id`,`suffix`,`first_name`,`last_name`,`middle_name`,`date_of_birth`,`issuing_authority`,`document_issuer`,`document_expiration_date`,`document_type`,`document_title`,`document_number`,`created_date`,`updated_date`,`applicant_id`) values (1,NULL,'first_name1','last_name1',NULL,'1988-11-26','NY','NY','2018-12-25','A','Drivers License','FF-234-123-1231','2016-06-09 18:07:30','2016-06-10 02:53:06',1),(2,NULL,'first_name2','last_name2',NULL,'1988-11-26','NY','NY','2018-12-25','A','Drivers License','FF-234-123-1231','2016-06-09 18:07:30','2016-06-10 02:53:08',2),(3,NULL,'first_name3','last_name3','middle3','1988-11-26','NY','NY','2018-12-25','A','Drivers License','FF-234-123-1231','2016-06-09 18:07:30','2016-06-10 02:53:10',3),(4,NULL,'first_name4','last_name4',NULL,'1988-11-26','NY','NY','2018-12-25','B','SSN Card','FF-234-123-1231','2016-06-09 18:07:30','2016-06-10 02:53:15',4),(5,NULL,'first_name5','last_name5',NULL,'1988-04-21','TX','TX','2018-06-06','B2','IDCard','FF-234-123-1232','2016-06-10 14:38:32','2016-06-10 14:38:41',4),(65536,NULL,'Frederick','Flintstone','J','1978-11-22','IL','IL','2018-04-13','A1','Passport','PP-465-67-6546','2016-06-10 15:11:50','2016-06-10 15:11:50',65536);
 
-/*Table structure for table `lifemedids` */
+/*Table structure for table `lifemedid_transactions` */
 
-DROP TABLE IF EXISTS `lifemedids`;
+DROP TABLE IF EXISTS `lifemedid_transactions`;
 
-CREATE TABLE `lifemedids` (
+CREATE TABLE `lifemedid_transactions` (
   `lifemed_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `transaction_id` varchar(256) NOT NULL,
   `person_id` varchar(100) NOT NULL,
@@ -93,9 +97,9 @@ CREATE TABLE `lifemedids` (
   PRIMARY KEY (`lifemed_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=65537 DEFAULT CHARSET=latin1;
 
-/*Data for the table `lifemedids` */
+/*Data for the table `lifemedid_transactions` */
 
-insert  into `lifemedids`(`lifemed_id`,`transaction_id`,`person_id`,`organization_id`,`time_stamp`,`updated`) values (1,'transaction_id10','person_id_1','org_id_1','2016-06-09 18:01:35','2016-06-09 18:01:35'),(2,'transaction_id11','person_id_2','org_id_2','2016-06-09 18:01:35','2016-06-09 18:01:35'),(3,'transaction_id12','person_id_3','org_id_3','2016-06-09 18:01:35','2016-06-09 18:01:35'),(4,'transaction_id13','person_id_4','org_id_4','2016-06-09 18:01:35','2016-06-09 18:01:35'),(65536,'b3a59d45-6fbb-1014-a7a4-dfe5ca3b7ee6','v2015','22042-3307','2016-06-10 15:11:50','2016-06-10 15:11:50');
+insert  into `lifemedid_transactions`(`lifemed_id`,`transaction_id`,`person_id`,`organization_id`,`time_stamp`,`updated`) values (1,'transaction_id10','person_id_1','org_id_1','2016-06-09 18:01:35','2016-06-09 18:01:35'),(2,'transaction_id11','person_id_2','org_id_2','2016-06-09 18:01:35','2016-06-09 18:01:35'),(3,'transaction_id12','person_id_3','org_id_3','2016-06-09 18:01:35','2016-06-09 18:01:35'),(4,'transaction_id13','person_id_4','org_id_4','2016-06-09 18:01:35','2016-06-09 18:01:35'),(65536,'b3a59d45-6fbb-1014-a7a4-dfe5ca3b7ee6','v2015','22042-3307','2016-06-10 15:11:50','2016-06-10 15:11:50');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
